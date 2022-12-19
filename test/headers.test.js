c0ecefadaeafdeae53a387aba6ec8c60e12b3bd9
Anusree <86919554+Anusreetk@users.noreply.github.com>
2022-12-19 17:10:32 +0530

    Update headers.test.js

--- test/headers.test.js
+++ test/headers.test.js
@@ -1,7 +1,17 @@
-import { html, fixture, expect } from '@open-wc/testing';
-import { stub } from 'sinon';
-import '../src/header/Header.js';
 
-describe('loan-header', () => {
-  // Write test cases inside this block
+import { html, fixture, expect } from '@open-wc/testing';
+import Sinon from "sinon";
+import {Header} from '../src/header/Header.js';
+var obj = new Header();
+describe.skip('header', () => {
+  it('is accessibile', async () => {
+    expect(obj).to.be.accessible();
+  });
+
+  it.skip('localeChanged', async () => {
+    const Spy = Sinon.spy(obj, 'localeChanged');
+    const Btn = await obj.shadowRoot.querySelector('button[id="en-GB"]');
+    Btn.simulate('click');
+    Sinon.assert.calledOnce(Spy);
+  });
 });
