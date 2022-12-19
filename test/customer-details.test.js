3dac319f4e7aa380ebbdf725507c0df818bd281d
Anusree <86919554+Anusreetk@users.noreply.github.com>
2022-12-19 17:08:40 +0530

    Update customer-details.test.js

--- test/customer-details.test.js
+++ test/customer-details.test.js
@@ -1,7 +1,21 @@
 import { html, fixture, expect } from '@open-wc/testing';
-import { stub } from 'sinon';
-import '../src/Customer/Customer-details.js';
+import Sinon from "sinon";
+import { CustomerDetails } from  '../src/Customer/Customer-details.js';
+var obj = new CustomerDetails();
 
 describe('customer details', () => {
-  // Write test cases inside this block
+  it('is Accessible', async () => {
+    expect(obj).to.be.accessible();
+  });
+  it('lion-input-amount',async()=>{
+    console.log("customer input field should not empty");
+    const el = await fixture(html`<customer-details> </customer-details>`);
+    const inputElement = el.shadowRoot.querySelector('lion-input-amount');
+    expect(inputElement.value).to.not.equal(" ");
+   });
+   it('_toEmidetails', () => {
+    const Spy = Sinon.spy(obj, '_toEmidetails');
+    obj._toEmidetails();
+    Sinon.assert.calledOnce(Spy);
+  });
 });
