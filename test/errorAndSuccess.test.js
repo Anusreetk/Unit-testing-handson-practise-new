a8aedd6fea3055a7fdc6d9c5eec4f6a40e3bdc2e
Anusree <86919554+Anusreetk@users.noreply.github.com>
2022-12-19 17:09:59 +0530

    Update errorAndSuccess.test.js

--- test/errorAndSuccess.test.js
+++ test/errorAndSuccess.test.js
@@ -1,12 +1,30 @@
-import { html, fixture, expect } from '@open-wc/testing';
-// import { stub } from 'sinon';
-import '../src/SuccessAndError/Success.js';
-import '../src/SuccessAndError/Error.js';
 
-describe('Success screen ', () => {
-  // Write test cases inside this block
+import { html, fixture, expect } from '@open-wc/testing';
+import Sinon from "sinon";
+import {LoanSuccess} from '../src/SuccessAndError/Success.js';
+import {LoanError} from '../src/SuccessAndError/Error.js';
+var obj1 = new LoanSuccess();
+var obj2 = new LoanError();
+
+describe('Success', () => {
+  it('is Accessible', async () => {
+    expect(obj1).to.be.accessible();
+  });
+
+  it(' success _toHome ', async () => {
+    const Spy = Sinon.spy(obj1, '_toHome');
+    obj1._toHome();
+    Sinon.assert.calledOnce(Spy);
+  });
 });
 
-describe('error screen', () => {
-  // Write test cases inside this block
+describe('Error', () => {
+  it('is Accessible', async () => {
+    expect(obj2).to.be.accessible();
+  });
+  it('error _toHome', async () => {
+    const Spy = Sinon.spy(obj2, '_toHome');
+    obj2._toHome();
+    Sinon.assert.calledOnce(Spy);
+  });
 });
